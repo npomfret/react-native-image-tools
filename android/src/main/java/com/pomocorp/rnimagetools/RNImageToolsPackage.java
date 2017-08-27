044577876966ceaf55e57c22d20523a1023c823f
Andy <flyandi@yahoo.com>
2017-08-26 23:39:56 -0700

    Correction: Only remove annotation for backwards

--- android/src/main/java/com/pomocorp/rnimagetools/RNImageToolsPackage.java
+++ android/src/main/java/com/pomocorp/rnimagetools/RNImageToolsPackage.java
@@ -16,6 +16,10 @@ public class RNImageToolsPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new RNImageToolsModule(reactContext));
     }
 
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
